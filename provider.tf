5e72fe2c2385c790fd8c5336154efe6e60d17f51
Navid8120 <151009079+Navid8120@users.noreply.github.com>
2024-12-24 11:30:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  access_key = AKIAYXWBOF2RHOU2XI4W
+  secret_key = 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
 }
